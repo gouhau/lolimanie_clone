7b4212f4ae233a4b6e8cf6a4d12800e5c0a83068
gouhau <120553364+gouhau@users.noreply.github.com>
2023-01-09 19:32:45 +0100

    Add files via upload

---
+++ door.gd
@@ -0,0 +1,6 @@
+extends Sprite
+
+var sus = 5
+
+func _physics_process(delta):
+		print(sus+1)
---
+++ key.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+func _on_Area2D_body_entered(body):
+	if body in get_tree().get_nodes_in_group("player"):
+		queue_free()
